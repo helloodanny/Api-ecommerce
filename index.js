2a6670b70bb04a15d60d74d5119e31cd907278de
Daniela Palacios <kanavarrete.dwfsc22@bootcamp.ucamp.io>
2024-03-14 22:05:54 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,25 @@ const app = express();
 const puerto = 3000;
 
 app.get('/', (req, res) => {
-  res.send('¡Hola, Express!');
+    res.send('¡Hola, Express!');
+});
+
+app.get('/products', (req, res) => {
+    res.send('GET/products');
+});
+
+app.post('/products', (req, res) => {
+    res.send('POST/products');
+});
+
+app.put('/products', (req, res) => {
+    res.send('PUT/products');
+});
+
+app.delete('/products', (req, res) => {
+    res.send('DELETE/products');
 });
 
 app.listen(puerto, () => {
-  console.log('Servidor escuchando en http://localhost:' + puerto);
+    console.log('Servidor escuchando en http://localhost:' + puerto);
 });

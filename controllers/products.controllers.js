bb7afe10f6892cd42b401da9ef189c0809fa9cea
Daniela Palacios <kanavarrete.dwfsc22@bootcamp.ucamp.io>
2024-03-22 22:40:00 -0600

    agregar y eliminar productos

--- controllers/products.controllers.js
+++ controllers/products.controllers.js
@@ -5,16 +5,16 @@ const Product = require('../models/Product.model');
 const productsGet = async (req = request, res = response) => {
 
     const id = req.query.id;
-    
+
     let products = [];
 
-    if (id){
+    if (id) {
 
 
-    
+
         products = await Product.findById(id);
     } else {
-        products = await Product.find ();
+        products = await Product.find();
     }
     res.status(200).json({
         message: 'Datos cargados correctamente',
@@ -26,28 +26,41 @@ const productsGet = async (req = request, res = response) => {
 
 const productsPost = async (req = request, res = response) => {
     const body = req.body;
-    
+
 
     let product = Product(body);
     await product.save();
 
     res.status(200).json({
         message: 'Product guardado',
-        data:product
+        data: product
     })
 
 }
 
-const productsPut = (req = request, res = response) => {
-    res.send('Put/products');
+const productsPut = async (req = request, res = response) => {
+    const { id } = req.query
 
+    const productoEditar = req.body
+
+    const productoActualizado = await Product.findByIdAndUpdate(id, productoEditar, { new: true })
+    res.status(200).json({
+        message: 'Producto actualizado correctamente',
+        code: productoActualizado
+    });
 }
 
 
-    const productsDelete = (req = request, res = response) => {
-        res.send('Delete/products');
-    
-    }
+const productsDelete = async (req = request, res = response) => {
+    const { id } = req.query;
+    await Product.findByIdAndDelete(id);
+ 
+    res.status(200).json({
+        message: 'Producto eliminado correctamente',
+        code: id
+    });
+
+}
 
 
 
